9097d8acc9b70b472a08b00fce9322da0d2f5a0d
mixomai123 <43234954+mixomai123@users.noreply.github.com>
2019-06-15 16:23:21 +0400

    Add files via upload

---
+++ tsv2txt.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import re
+myText=""
+def myStrip(word):
+    word.replace("	","");
+    return word
+with open("test1.txt") as file_to_read:
+    for line in file_to_read:
+        if not "<" in line[0]:
+            token=line.split("	")[0];
+            if re.match('^[ა-ჰa-zA-Z0-9_]+$',token):
+                token=" "+token
+            myText+=token;
+print(myText);
